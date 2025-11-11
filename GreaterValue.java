6c5e46bae60d510d35b58ff2e38ac2307d2de8e9
Abdul Kabir <abdulkabir0717@gmail.com>
2025-11-12 02:46:21 +0530

    GreaterValue program

---
+++ GreaterValue.java
@@ -0,0 +1,10 @@
+public class GreaterValue {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+
+        int greater = (a > b) ? a : b;
+
+        System.out.println("Greater Number is: " + greater);
+    }
+}
--- Range.java
+++ Range.java
@@ -1,20 +1,18 @@
 class Range
 {
-  public static void main(Strings args[])
+  public static void main(String args[])
   {
     System.out.println(Byte.MIN_VALUE);
     System.out.println(Byte.MAX_VALUE);
     System.out.println(Short.MIN_VALUE);
     System.out.println(Short.MAX_VALUE);
     System.out.println(Integer.MIN_VALUE);
-    System.out.println(Integer.MAX_VALUE);
-    System.out.println(long.MIN_VALUE);
-    System.out.println(Long.MAX_VALUE);
+    System.out.println(Integer.MAX_VALUE); 
+    System.out.println(long.MIN_VALUE);
+    System.out.println(Long.MAX_VALUE);
     System.out.println(Double.MIN_VALUE);
-    System.out.println(Double.MAX_VALUE);
-    System.out.println(Float.MIN_VALUE);
-    System.out.println(Float.MAX_VALUE);
-
+    System.out.println(Double.MAX_VALUE);
+    System.out.println(Float.MIN_VALUE);
+    System.out.println(Float.MAX_VALUE);
   }
-
 }
