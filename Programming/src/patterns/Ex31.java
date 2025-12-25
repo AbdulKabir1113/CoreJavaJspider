da81a51d3f29481fa3f384fa976d763016b5122e
Abdul Kabir <abdulkabir0717@gmail.com>
2025-12-25 17:50:16 +0530

    NamePattern

---
+++ Programming/src/patterns/Ex31.java
@@ -0,0 +1,50 @@
+package patterns;
+
+public class Ex31 {
+
+public static void main(String[] args) {
+		
+		for(int i=1; i<=7; i++) {
+			
+			for(int j=1; j<=5; j++) {
+				if(j==1 || (i<=4 && i+j==6) || (i-j==2 && i>=5) ) System.out.print("*");
+				else System.out.print(" ");
+			}
+			System.out.print("  ");
+		
+		    //A
+			for(int j=1; j<=5; j++) {
+				if(i+j==4 || j-i ==2 || i==4 || j==1  && i>=3 || j==5 && i>=3)
+					System.out.print("* ");
+				else System.out.print("  ");
+			}
+            System.out.print("  ");
+		
+		    //B
+			for(int j=1; j<=5; j++) {
+				if(j==1 || i==1 && j<5 || i==2 && j==5 || i==3 && j==5 || i==4 && j<5 || (i==7 && j<=4) || (i==5 && j==5) || (i==6 && j==5) )
+					System.out.print("* ");
+				else System.out.print("  ");
+			}
+		    System.out.print("  ");
+		    
+		
+		    //I
+			for(int j=1; j<=5; j++) {
+				if(i==1 || i==7 || j==3 )
+					System.out.print("* ");
+				else System.out.print("  ");
+			}
+			System.out.print("  ");
+		
+		
+		    //R
+			for(int j=1; j<=5; j++) {
+				if(j==1 || i==1 && j<5 || i==2 && j==5 || i==3 && j==5 || i==4 && j<5 || i-j==2 )
+					System.out.print("* ");
+				else System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+}
