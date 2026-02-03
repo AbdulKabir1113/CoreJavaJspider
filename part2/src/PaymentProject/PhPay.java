ac7eebe1dafad40a428c5f9878cead04a653a5c0
Abdul Kabir <abdulkabir0717@gmail.com>
2026-02-03 19:44:45 +0530

    PaymentAppProject

---
+++ part2/src/PaymentProject/PhPay.java
@@ -0,0 +1,5 @@
+package PaymentProject;
+
+public class PhPay {
+
+}
--- part2/src/encapsulation/Dog.java
+++ part2/src/encapsulation/Dog.java
@@ -5,6 +5,7 @@ public class Dog {
 	String breed;
 	int age;
 
+	
 public Dog(String breed, int age) {
 	this.breed=breed;
 	this.age=age;
